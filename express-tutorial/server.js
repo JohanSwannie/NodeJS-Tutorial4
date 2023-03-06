bdc12ba2e69d66bd7ffb662346d99cef3c569c26
jswan <jswannieman@gmail.com>
2023-03-06 13:01:54 +1100

    Update server Javascript Component

--- express-tutorial/server.js
+++ express-tutorial/server.js
@@ -16,6 +16,37 @@ app.get("/old-page(.html)?", (request, response) => {
   response.redirect(301, "/new-page.html");
 });
 
+// Route handlers
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("attempted to load hello.html");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello World!");
+  }
+);
+
+// chaining route handlers
+
+const firstRoute = (request, response, next) => {
+  console.log("First Route");
+  next();
+};
+
+const secondRoute = (request, response, next) => {
+  console.log("Second Route");
+  next();
+};
+
+const thirdRoute = (request, response) => {
+  console.log("Third Route");
+  response.send("All Routes Completed Successfully");
+};
+
+app.get("/chainThem(.html)?", [firstRoute, secondRoute, thirdRoute]);
+
 app.get("/*", (request, response) => {
   response.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
